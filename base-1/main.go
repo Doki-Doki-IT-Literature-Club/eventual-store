d3b2b5971a41e9fa4dc49508c10084b09dc3110e
Daniil Golovin <golovindaniely@gmail.com>
2025-05-01 17:42:53 +0200

    shipment

--- base-1/main.go
+++ base-1/main.go
@@ -1,115 +1,113 @@
 package main
 
 import (
-	"base/pkg/exmpl"
-	"bytes"
 	"context"
-	"fmt"
-	"io"
+	"encoding/json"
 	"log"
-	"net/http"
 	"time"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
-var base2Address = "http://base-2-base-2-1:8002"
-var kafkaAddress = "kafka:9092"
-var dbConnString = "postgres://user:password@postgres:5432/mydb"
+const base2Address = "http://base-2-base-2-1:8002"
+const kafkaAddress = "kafka:9092"
+const dbConnString = "postgres://user:password@postgres:5432/mydb"
+const orderShippingRequestTopic = "order-shipping-request"
+const orderShippingStatusTopic = "order-shipping-status"
 
-func connectToDB() (*pgx.Conn, error) {
-	conn, err := pgx.Connect(context.Background(), dbConnString)
-	if err != nil {
-		return nil, fmt.Errorf("unable to connect to database: %v", err)
-	}
-	return conn, nil
+type OrderShippingRequest struct {
+	OrderID string `json:"order_id"`
 }
 
-func createTestTable(conn *pgx.Conn) error {
-	_, err := conn.Exec(context.Background(), "CREATE TABLE IF NOT EXISTS test_table_base_1 (id SERIAL PRIMARY KEY, data TEXT)")
-	if err != nil {
-		return fmt.Errorf("unable to create table: %v", err)
-	}
-	return nil
+type OrderShippingStatus struct {
+	OrderID string `json:"order_id"`
+	Status  string `json:"status"`
 }
 
 func main() {
-	conn, err := connectToDB()
-	if err != nil {
-		log.Fatalf("Error connecting to database: %v", err)
-	}
-	defer conn.Close(context.Background())
-
-	if err := createTestTable(conn); err != nil {
-		log.Fatalf("Error creating test table: %v", err)
-	}
+	log.Printf("Starting base-1 service")
 
 	kcl, err := kgo.NewClient(
 		kgo.SeedBrokers(kafkaAddress),
+		kgo.AllowAutoTopicCreation(),
+		kgo.ConsumerGroup("shipping-group"),
+		kgo.ConsumeTopics(orderShippingRequestTopic),
 	)
 
 	if err != nil {
 		log.Fatalf("Error creating Kafka client: %v", err)
 	}
 
-	httpServer(kcl)
-}
+	defer kcl.Close()
 
-func httpServer(kcl *kgo.Client) {
-	client := &http.Client{
-		Timeout: 10 * time.Second,
-	}
+	ctx := context.Background()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("404 Not Found"))
-			return
+	for {
+		events := kcl.PollFetches(ctx)
+		if err := events.Err(); err != nil {
+			log.Printf("Error fetching events: %v", err)
+			continue
 		}
 
-		log.Printf("Received request: %s %s", r.Method, r.URL.Path)
+		events.EachRecord(func(r *kgo.Record) {
+			log.Printf("Processing")
 
-		req, err := http.NewRequest(http.MethodPost, base2Address, bytes.NewBufferString(exmpl.A))
-		if err != nil {
-			log.Printf("Error creating request: %v", err)
-			http.Error(w, "Error creating request", http.StatusInternalServerError)
-			return
-		}
-		req.Header.Set("Content-Type", "text/plain")
-
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Printf("Error sending request to base-2: %v", err)
-			http.Error(w, "Error sending request to base-2", http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Printf("Error reading response: %v", err)
-			http.Error(w, "Error reading response", http.StatusInternalServerError)
-			return
-		}
-
-		ctx := context.Background()
-		record := &kgo.Record{Topic: "received", Value: body}
-		kcl.Produce(ctx, record, func(_ *kgo.Record, err error) {
-			if err != nil {
-				fmt.Printf("record had a produce error: %v\n", err)
+			orderShippingRequest := &OrderShippingRequest{}
+			if err := json.Unmarshal(r.Value, orderShippingRequest); err != nil {
+				log.Printf("Error unmarshalling record: %v", err)
+				return
 			}
+			time.Sleep(time.Second * 5)
+
+			orderShippingStatusInDelivery := &OrderShippingStatus{
+				OrderID: orderShippingRequest.OrderID,
+				Status:  "In Delivery",
+			}
+
+			orderShippingStatusDeliveryBytes, err := json.Marshal(orderShippingStatusInDelivery)
+			if err != nil {
+				log.Printf("Error marshalling order shipping status: %v", err)
+				return
+			}
+
+			record := &kgo.Record{
+				Topic: orderShippingStatusTopic,
+				Value: orderShippingStatusDeliveryBytes,
+			}
+
+			kcl.Produce(ctx, record, func(_ *kgo.Record, err error) {
+				if err != nil {
+					log.Printf("record had a produce error: %v\n", err)
+				}
+
+				log.Printf("Produced record to topic %s", orderShippingStatusTopic)
+			})
+
+			time.Sleep(time.Second * 10)
+
+			orderShippingStatusDelivered := &OrderShippingStatus{
+				OrderID: orderShippingRequest.OrderID,
+				Status:  "Delivered",
+			}
+
+			orderShippingStatusDeliveredBytes, err := json.Marshal(orderShippingStatusDelivered)
+			if err != nil {
+				log.Printf("Error marshalling order shipping status: %v", err)
+				return
+			}
+
+			record = &kgo.Record{
+				Topic: orderShippingStatusTopic,
+				Value: orderShippingStatusDeliveredBytes,
+			}
+
+			kcl.Produce(ctx, record, func(_ *kgo.Record, err error) {
+				if err != nil {
+					log.Printf("record had a produce error: %v\n", err)
+				}
+
+				log.Printf("Produced record to topic %s", orderShippingStatusTopic)
+			})
 		})
-
-		w.WriteHeader(resp.StatusCode)
-		w.Write(body)
-		log.Printf("Response from base-2: %s", string(body))
-	})
-
-	port := 8001
-	log.Printf("Starting server on port %d", port)
-	log.Printf("Ready to send %q to %q", exmpl.A, base2Address)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
-		log.Fatalf("Server error: %v", err)
 	}
 }
